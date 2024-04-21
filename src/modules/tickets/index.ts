2e699b2d795baa60e7a5ca134b326718046d22be
inuoshios <inuoshios@gmail.com>
2024-04-21 01:08:17 +0100

    fix(ticket): add endpoints

--- src/modules/tickets/index.ts
+++ src/modules/tickets/index.ts
@@ -7,9 +7,9 @@ const router = Router();
 const handler = new TicketController();
 
 router.post("/ticket/create-ticket", validationSchema(bookTicketSchema), handler.bookTicket);
-router.post("/ticket/view-tickets", handler.bookTicket);
-router.post(
-  "/ticket/create-ticket",
+router.get("/ticket/view-tickets", handler.bookTicket);
+router.get(
+  "/ticket/check-ticket-status",
   validationSchema(checkTicketStatusSchema),
   handler.checkTicketStatus
 );
